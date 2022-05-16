8ca5207a66f65183a78ce24b57f62bf28eafa078
Fabian Florez <johndoe@example.com>
2022-05-16 08:51:43 -0500

    Agregamos el framework bootstrap

---
+++ public/global/global/js/main.js
@@ -0,0 +1,24 @@
+
+window.onload = function(){
+  // Funcion para utilizar los tooltips
+  document.querySelectorAll("[data-bs-toggle='tooltip']").forEach(e => 
+    new bootstrap.Tooltip(e, {
+      boundary: document.body
+    })
+    
+  )
+  //Para las ventanas de eliminar
+  var modal = document.getElementById('deleteModal')
+  modal.addEventListener('show.bs.modal',function(event){
+      console.log('Hola desde main')
+      var button = event.relatedTarget
+      var name =  button.getAttribute('data-bs-name')
+      var id =  button.getAttribute('data-bs-id')
+      var url = button.getAttribute('data-bs-url')
+      modal.querySelector(".modal-title span").innerHTML = name
+      modal.querySelector("#deleteProduct").setAttribute("href",url+id)
+  })
+  
+  
+  
+}
